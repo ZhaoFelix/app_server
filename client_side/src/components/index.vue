b32614d95b89057c020d86d8725e6536c6693bab
Felix <15021179915@163.com>
2019-03-14 18:41:05 +0800

    客户端

---
+++ client_side/src/components/index.vue
@@ -0,0 +1,59 @@
+<template lang="html">
+  <div class="my-container">
+     <el-row class="main">
+       <mavon-editor
+        class="my-editor"
+        v-model="content"
+        defaultOpen = "preview"
+        :ishljs="true"
+        :scrollStyle="false"
+        :subfield = "false"
+        codeStyle="dark"
+        :toolbarsFlag="false"
+        />
+     </el-row>
+   </div>
+</template>
+
+<script>
+import qs from 'qs';
+export default {
+  name:"index",
+  data(){
+    return {
+      content:''
+    }
+  },
+  created:function(){
+    var _this = this;
+    const id = this.$route.params.id;
+    console.log(id);
+    if (id==0){
+    }
+    else {
+      this.$axios.post('http://www.bedeveloper.cn:3000/list/querySingle',qs.stringify({
+        id:id
+      }))
+      .then(function(response){
+        var re = response.data;
+        if(re.code==0){
+         _this.content=re.result[0].content;
+        }
+        else{
+
+        }
+      })
+      .catch(function(err){
+
+      })
+    }
+  }
+}
+</script>
+
+<style lang="css">
+.my-container {
+  width: 375px;
+
+}
+</style>
---
+++ client_side/src/main.js
@@ -0,0 +1,23 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import 'element-ui/lib/theme-chalk/index.css'
+import 'mavon-editor/dist/css/index.css'
+import router from './router'
+import ElementUI from 'element-ui'
+import mavonEditor from 'mavon-editor'
+import Axios from 'axios'
+Vue.prototype.$axios = Axios;
+
+Vue.config.productionTip = false
+Vue.use(ElementUI);
+Vue.use(mavonEditor)
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
